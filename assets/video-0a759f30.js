32412ec52610a1669377b6fe756251ff08fc9c7f
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-03-30 12:22:34 +0000

    Deploying to gh-pages from @ Dezertirr/project-Outline@2ad7518baef0119e0f42e5764deb131f2a654d93 🚀

---
+++ assets/video-0a759f30.js
@@ -0,0 +1,1 @@
+(()=>{const e={openVideoBtn:document.querySelector("[data-video-open]"),closeVideoBtn:document.querySelector("[data-video-close]"),video:document.querySelector("[data-video]")};e.openVideoBtn.addEventListener("click",o),e.closeVideoBtn.addEventListener("click",o);function o(){e.video.classList.toggle("video-is-hidden")}})();
