aefdbb094d8dd9d2b71f35b5b60d223c9e0e34c5
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-04-01 21:03:09 +0000

    Deploying to gh-pages from @ Dezertirr/project-Outline@5aea63ca2cce9fa558bdb306bcd7f30ad88545ba 🚀

--- assets/buy-now-5163ecb2.js
+++
@@ -1,1 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[sellers-data-modal-open]"),closeModalBtn:document.querySelector("[sellers-data-modal-close]"),modal:document.querySelector("[sellers-data-modal]")};e.openModalBtn.addEventListener("click",l),e.closeModalBtn.addEventListener("click",l);function l(){e.modal.classList.toggle("is-hidden")}})();
